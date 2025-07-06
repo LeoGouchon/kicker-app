9e37c10ed859e7667a7cf3723757df1735a0791a
Léo Gouchon <leo.gouchon@gmail.com>
2025-07-06 21:29:15 +0200

    remove .env files from tracking

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+    readonly VITE_IS_ADMIN: 'true' | 'false';
+    readonly VITE_MODE: 'dev' | 'prod';
+    readonly VITE_BACKEND_API_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
