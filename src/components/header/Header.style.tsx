16b13c3632c4fbb4f020c55080499aacf8a9a88e
Léo Gouchon <leo.gouchon@gmail.com>
2025-07-21 20:15:43 +0200

    styled(header): change UI header

--- src/components/header/Header.style.tsx
+++ src/components/header/Header.style.tsx
@@ -2,10 +2,15 @@ import {Layout} from "antd";
 import styled from "@emotion/styled";
 
 export const StyledHeader = styled(Layout.Header)`
-    background-color: inherit;      
+    background-color: var(--ant-color-bg-container);      
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom-left-radius: var(--ant-border-radius-lg);
+    border-bottom-right-radius: var(--ant-border-radius-lg);
     
     gap: 16px;
+    margin-bottom: 8px;
+    margin-left: 16px;
+    margin-right: 16px;
 `;
