5d5b7ff3fee07aba11ff6e50ec6720fb3fe007c9
Léo Gouchon <leo.gouchon@gmail.com>
2025-07-27 23:23:40 +0200

    feat(rank): add reset elo button for admin

--- src/components/header/components/logged/Logged.tsx
+++ src/components/header/components/logged/Logged.tsx
@@ -1,41 +1,64 @@
-import {Button, Flex, Popover} from 'antd';
-import {type ReactElement, useContext} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { Button, Flex, message, Popover } from 'antd';
+import { type ReactElement, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import {UserContext} from '../../../../context/UserContext.tsx';
-import {useLogout} from '../../../../hooks/useApiEndPoint/useLogout.ts';
-import {ROUTES} from '../../../../routes/constant.ts';
-import type {UserType} from '../../../../types/User.type.ts';
-import {ClickyAvatar} from './Logged.style.tsx';
+import { UserContext } from '../../../../context/UserContext.tsx';
+import { useLogout } from '../../../../hooks/useApiEndPoint/useLogout.ts';
+import { useResetElo } from '../../../../hooks/useApiEndPoint/useStats.ts';
+import { ROUTES } from '../../../../routes/constant.ts';
+import type { UserType } from '../../../../types/User.type.ts';
+import { ClickyAvatar } from './Logged.style.tsx';
 
 export const Logged = () => {
-    const {user}: { user?: UserType } = useContext(UserContext);
+    const { user }: { user?: UserType } = useContext(UserContext);
     const logoutMutation = useLogout();
     const navigate = useNavigate();
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const resetEloMutation = useResetElo();
 
     const handleLogout = () => {
         logoutMutation.mutate(undefined, {
             onSuccess: () => {
                 window.location.reload();
-            }
+            },
         });
     };
 
-    const popoverContent: ReactElement =
+    const handleResetELO = () => {
+        resetEloMutation.mutate(undefined, {
+            onSuccess: () => {
+                messageApi.open({
+                    type: 'success',
+                    content: 'Recalcul des ELOs effectué avec succès !',
+                });
+            },
+        });
+    };
+
+    const popoverContent: ReactElement = (
         <Flex vertical gap={'small'}>
             {user?.email}
-            {user?.admin &&
-                <Button onClick={() => navigate(ROUTES.INVITE)}>Inviter</Button>}
-            <Button onClick={handleLogout} danger>Se déconnecter</Button>
-        </Flex>;
+            {user?.admin && (
+                <>
+                    <Button loading={resetEloMutation.isPending} onClick={handleResetELO}>
+                        Recalculer les ELOs
+                    </Button>
+                    <Button onClick={() => navigate(ROUTES.INVITE)}>Inviter</Button>
+                </>
+            )}
+            <Button onClick={handleLogout} danger>
+                Se déconnecter
+            </Button>
+        </Flex>
+    );
 
     return (
-        <Popover
-            content={popoverContent}
-            placement={'bottom'}
-            trigger={'click'}
-        >
-            <ClickyAvatar>{user?.email.slice(0, 1).toUpperCase()}</ClickyAvatar>
-        </Popover>
+        <>
+            <Popover content={popoverContent} placement={'bottom'} trigger={'click'}>
+                <ClickyAvatar>{user?.email.slice(0, 1).toUpperCase()}</ClickyAvatar>
+            </Popover>
+            {contextHolder}
+        </>
     );
 };
--- src/hooks/useApiEndPoint/useStats.ts
+++ src/hooks/useApiEndPoint/useStats.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import type { GlobalStats } from '../../types/GlobalStats.type.ts';
 import { api } from '../../utils/api.ts';
@@ -9,6 +9,19 @@ export const useGetGlobalStats = () => {
         queryFn: async (): Promise<GlobalStats[]> => {
             const res = await api.get('/kicker/stats/global');
             return res.data;
+        },
+    });
+};
+
+export const useResetElo = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async () => {
+            await api.post('/kicker/matches/recalculate-elo');
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['global-stats'], exact: false });
         },
     });
 };
