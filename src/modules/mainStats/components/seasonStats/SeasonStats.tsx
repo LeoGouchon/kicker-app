1dd82479e2818b4cd19e3d03ccc4fe684f87bfad
Léo Gouchon <leo.gouchon@gmail.com>
2025-12-15 20:00:25 +0100

    fix(ranking): unranked player could not be select to visit profil

--- src/modules/mainStats/components/seasonStats/SeasonStats.tsx
+++ src/modules/mainStats/components/seasonStats/SeasonStats.tsx
@@ -205,7 +205,7 @@ export const SeasonStats = React.memo(
                                     : '';
                                 return (
                                     <LinkTypographyStyled>
-                                        <Link to={`${ROUTES.PLAYER}/${record.id}`} style={{ all: 'unset' }}>
+                                        <Link to={`${ROUTES.PLAYER}/${record.playerId}`} style={{ all: 'unset' }}>
                                             {firstname} {lastname}
                                         </Link>
                                     </LinkTypographyStyled>
