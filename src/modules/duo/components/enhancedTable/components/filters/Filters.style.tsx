644f50d57f1f65cb8a5798486cd78eb0dbb0ac68
l.gouchon <leo.gouchon@gmail.com>
2026-05-04 21:14:09 +0200

    wip(duo): better UI

---
+++ src/modules/duo/components/enhancedTable/components/filters/Filters.style.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Flex } from 'antd';
+
+export const GlobalWrapper = styled(Flex)`
+    background-color: var(--ant-color-bg-layout);
+    padding: 8px;
+    border-radius: var(--ant-border-radius-lg);
+`;
