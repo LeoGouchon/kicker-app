a3f7295718460ba7dd0d75ac5d975c001c1ede1b
LeoGouchon <72149969+LeoGouchon@users.noreply.github.com>
2026-06-04 18:43:04 +0200

    chore(sider): store collapsed side menu props inside localstorage (#19)

--- src/components/sider/Sider.tsx
+++ src/components/sider/Sider.tsx
@@ -7,16 +7,27 @@ import { useLocation } from 'react-router-dom';
 import { type MenuItem, useGetMenuItemElements } from '../../hooks/useMenuItems.tsx';
 import { StyledSider } from './Sider.style.tsx';
 
+const IS_NAV_LEFT_BAR_OPEN_STORAGE_KEY = 'isNavLeftBarOpen';
+
 export const Sider = () => {
-    const [isCollapsed, setIsCollapsed] = useState(true);
+    const [isNavLeftBarOpen, setIsNavLeftBarOpen] = useState(
+        () => localStorage.getItem(IS_NAV_LEFT_BAR_OPEN_STORAGE_KEY) === 'true'
+    );
     const menuItems: MenuItem[] = useGetMenuItemElements();
 
     const location = useLocation();
 
     const selectedKey = menuItems.find((item) => location.pathname.startsWith(item?.key as string))?.key;
 
+    const handleToggleNavLeftBar = () => {
+        const newIsNavLeftBarOpen = !isNavLeftBarOpen;
+
+        localStorage.setItem(IS_NAV_LEFT_BAR_OPEN_STORAGE_KEY, String(newIsNavLeftBarOpen));
+        setIsNavLeftBarOpen(newIsNavLeftBarOpen);
+    };
+
     return (
-        <StyledSider collapsible defaultCollapsed collapsedWidth={60} trigger={null} collapsed={isCollapsed}>
+        <StyledSider collapsible defaultCollapsed collapsedWidth={60} trigger={null} collapsed={!isNavLeftBarOpen}>
             <div style={{ height: '100%', paddingBottom: '32px' }}>
                 <div style={{ height: '100%' }}>
                     <Menu
@@ -32,13 +43,13 @@ export const Sider = () => {
                         type="primary"
                         style={{ backgroundColor: 'transparent', border: 'none', paddingBottom: '8px' }}
                         icon={
-                            isCollapsed ? (
-                                <FontAwesomeIcon icon={faArrowRight} />
-                            ) : (
+                            isNavLeftBarOpen ? (
                                 <FontAwesomeIcon icon={faArrowLeft} />
+                            ) : (
+                                <FontAwesomeIcon icon={faArrowRight} />
                             )
                         }
-                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        onClick={handleToggleNavLeftBar}
                     />
                 </div>
             </div>
